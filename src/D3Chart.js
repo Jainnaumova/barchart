5e9c7b9a8cc34d7f344bb1e13fcc36f038be12dd
Evgeniya Naumova <jainnaumova@gmail.com>
2019-11-15 09:49:52 -0500

    Fix hardcord bars height and bars start from bottom

--- src/D3Chart.js
+++ src/D3Chart.js
@@ -1,26 +1,28 @@
 import * as d3 from 'd3';
 
 const url = 'https://udemy-react-d3.firebaseio.com/tallest_men.json';
+const WIDTH = 800;
+const HEIGHT = 500;
 
 export default class D3Chart {
   constructor(element) {
     const svg = d3.select(element)
       .append('svg')
-        .attr('width', 800)
-        .attr('height', 500)
+        .attr('width', WIDTH)
+        .attr('height', HEIGHT)
 
     d3.json(url).then(data => {
       // implementing scaling bars by axios y
       const y = d3.scaleLinear()
         // the highest height
-        .domain([0, 272])
-        .range([0, 500])
+        .domain([0, d3.max(data, d => d.height)])
+        .range([0, HEIGHT])
         // console.log(y(100)) // return aprox 183
 
       // implementing scaling bars by axios x
       const x = d3.scaleBand()
         .domain(data.map(d => d.name))
-        .range([0, 800])
+        .range([0, WIDTH])
         .padding(0.4)
 
       const rects = svg.selectAll('rect')
@@ -28,7 +30,7 @@ export default class D3Chart {
 
       rects.enter().append('rect')
         .attr('x', d => x(d.name)) // using scaling function x
-        .attr('y', 0)
+        .attr('y', d => HEIGHT - y(d.height)) // start bars from bottom
         .attr('width', x.bandwidth)
         .attr('height', d => y(d.height)) // using scaling function y
         .attr('fill', 'grey')
