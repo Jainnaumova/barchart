798180bbdb404bc20074a2aaaaf1a27efa9db1fb
Evgeniya Naumova <jainnaumova@gmail.com>
2019-11-17 20:05:33 -0500

    Add tracking dropdown

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -6,26 +6,34 @@ import Col from 'react-bootstrap/Col';
 import ChartWrapper from './ChartWrapper';
 import GenderDropDown from './GenderDropDown';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar bg="light">
-        <Navbar.Brand>Barchartly</Navbar.Brand>
-      </Navbar>
-      <Container>
-        <Row>
-          <Col xs={12}>
-            <GenderDropDown />
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={12}>
-            <ChartWrapper />
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
+class App extends Component {
+  state = {
+    gender: 'men'
+  }
+
+  genderSelected = (gender) => this.setState({ gender })
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar bg="light">
+          <Navbar.Brand>Barchartly</Navbar.Brand>
+        </Navbar>
+        <Container>
+          <Row>
+            <Col xs={12}>
+              <GenderDropDown genderSelected={this.genderSelected}/>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12}>
+              <ChartWrapper />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
